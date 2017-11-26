34eb8b193631098febdf8e7b1c28d530593a91e9
Adrian Bella <bella.adus@gmail.com>
2017-11-26 13:23:39 +0100

    Events page UI

--- Timetable/TimeTableASP/Pages/Events.cshtml.cs
+++ Timetable/TimeTableASP/Pages/Events.cshtml.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TimetableInterfaces.Models;
 
 namespace TimeTableASP.Pages
 {
     public class EventsModel : PageModel
     {
-        
-        public void OnGet()
+
+        private readonly TimetableInterfaces.Interfaces.ICalendarService _ics;
+
+        public EventsModel(TimetableInterfaces.Interfaces.ICalendarService ics)
         {
-            
+            _ics = ics;
+        }
+
+        public IList<Event> Event { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Event =  _ics.ListEvents("Adrian");
         }
     }
 }
